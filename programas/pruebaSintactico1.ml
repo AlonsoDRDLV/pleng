de520a8ca4ed35873624bc35cbf8a77cf3d8304e
AlonsoDRDLV <71894461+AlonsoDRDLV@users.noreply.github.com>
2021-03-30 19:01:21 +0200

    prueba sintactico 1 (comas)

---
+++ programas/pruebaSintactico1.ml
@@ -0,0 +1,52 @@
+%% Prueba que detecta como error la falta de comas entre identificadores
+listas de parametros listas de asignables y listas de escribibles %%
+%-----------------------------------------------------------
+Programa maximo_comun_divisor;
+%-----------------------------------------------------------
+
+%Error 1: falta la coma entre m y d
+entero m d, a, b;
+
+%-----------------------------------------------------------
+accion dato;
+%-----------------------------------------------------------
+Principio
+        d:=0;
+        Mq d<=0
+                escribir("Escribe un numero: ");
+                leer(d);
+                Si d<=0 ent
+                 	 %Error 2: falta la coma entre i y j
+                        leer(i j);
+                        %Error 3: falta la coma entre entacar (13) y entacar (13) 
+                        escribir (entacar (13) entacar (10));
+                FSi
+        FMq
+Fin
+
+%-----------------------------------------------------------
+%Error 4: falta la coma entre a y b 
+accion mcd(ref entero a b);
+%-----------------------------------------------------------
+entero r;
+Principio
+        r:=a mod b;
+        Mq r<>0
+                a:=b;
+                b:=r;
+                r:=a mod b;
+        FMq
+		m := b;
+Fin
+
+%-----------------------------------------------------------
+Principio
+        dato;
+        a := d;
+        dato;
+        b := d;
+        mcd;
+        escribir("El MCD es: ", m);
+        escribir (entacar (13), entacar (10));
+Fin
+
