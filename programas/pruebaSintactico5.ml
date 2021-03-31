8a6436629febc86326b1af83985d4e4da6e6b11e
AlonsoDRDLV <71894461+AlonsoDRDLV@users.noreply.github.com>
2021-03-31 18:36:09 +0200

    cabecera

--- programas/pruebaSintactico5.ml
+++ programas/pruebaSintactico5.ml
@@ -1,7 +1,10 @@
+%Error 1: Se esperaba identificador tras tPROGRAMA pero se ha leido otra cosa
+%Error 2: Se ha encontrado un identificador y se esperaba un operador relacional, multiplicativo o aditivo
+
 programa
 
 Principio
-    %% Error 1: Se ha encontrado un identificador y se esperaba un operador relacional, multiplicativo o
+    %% Error 2: Se ha encontrado un identificador y se esperaba un operador relacional, multiplicativo o
     aditivo %%
     j := 5 k 5;
     f := 6
