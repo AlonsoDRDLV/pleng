2a5a5e62c618750e1aedf01b756a62a563da05ff
fabic <fabic@DESKTOP-MJLQF8G>
2021-03-31 17:11:52 +0200

    pruebas programa

---
+++ programas/pruebaSintactico5.ml
@@ -0,0 +1,14 @@
+programa
+
+Principio
+    %% Error 1: Se ha encontrado un identificador y se esperaba un operador relacional, multiplicativo o
+    aditivo %%
+    j := 5 k 5;
+    f := 6
+    g := 8;
+
+    entero c, v, caracter;
+
+fin
+
+
